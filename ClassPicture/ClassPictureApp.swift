3785132fcfcd3dba937b94009a1390c01f582143
altugtaskiran <altugtaskiran@gmail.com>
2024-03-12 19:02:38 +0300

    Initial Commit

---
+++ ClassPicture/ClassPictureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassPictureApp.swift
+//  ClassPicture
+//
+//  Created by Altuğ Taşkıran on 12.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ClassPictureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
